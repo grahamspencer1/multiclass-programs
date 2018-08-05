1b209787b6c891c729509a26f632f1f6239c597f
Graham Spencer <grahamspence@gmail.com>
2018-08-05 14:09:38 -0400

    testing connection

---
+++ product.rb
@@ -0,0 +1,16 @@
+class Product
+
+  attr_accessor :name, :base_price, :tax_rate
+
+  def initialize(name, base_price, tax_rate)
+    @name = name
+    @base_price = base_price
+    @tax_rate = tax_rate
+  end
+
+end
+
+
+name
+base_price
+tax_rate
